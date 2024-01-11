5bd4930fb444d7b7b804826b3f7b1465a466c9e3
André Rosatto <linus1178@gmail.com>
2024-01-11 14:16:14 -0300

    layout completo

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('#form-title-wrapper img').addEventListener('click', () => {
+	console.log('ok');
+	document.querySelector('#form-section').classList.toggle('collapsed');
+});
